d19de0b755d9881b54b9d20c591d5f0de4f03d20
ctgu-huamei <1476964577@qq.com>
2022-03-07 17:05:37 +0800

    add L006,L0521,L0504.py

---
+++ L0006.py
@@ -0,0 +1,56 @@
+'''
+6. Z 字形变换
+
+将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行Z 字形排列。
+比如输入字符串为 "PAYPALISHIRING"行数为 3 时，排列如下：
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+请你实现这个将字符串进行指定行数变换的函数：
+
+string convert(string s, int numRows);
+
+示例 1：
+
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+示例 2：
+输入：s = "PAYPALISHIRING", numRows = 4
+输出："PINALSIGYAHRPI"
+解释：
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zigzag-conversion
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def convert(self, s: str, numRows: int) -> str:
+        '''
+        Z字变换
+        '''
+        if numRows<2:
+            return s
+        status = -1
+        i = 0
+        arr = ['' for j in range(numRows)]
+        for ch in s:
+            arr[i] += ch
+            if i == 0 or i == numRows - 1:
+                status = - status
+            i += status
+        return ''.join(arr)
+
+
+m = Solution()
+print(m.convert('PAYPALISHIRING', 3))
